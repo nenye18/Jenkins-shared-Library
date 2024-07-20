06a3b232d9a6458101d42545e9ac63832e933dd7
Chinenye Nwagbara <chinenye.nw@gmail.com>
2024-07-20 07:03:08 -0500

    Updated buildImage.groovy
    The groovy file will be describing the nodejs pipeline

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,8 +1,8 @@
 def call(){
-    echo 'building the docker image..'
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo',passwordVariable: 'PASS', usernameVariable: 'USER')]){
-        sh 'docker build -t cnwagba/jenkins-repo-dockerhub:node-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh  'docker push cnwagba/jenkins-repo-dockerhub:node-2.0'
-
+    echo 'building the docker image...
+    withCredentials([usernamePassword(credentialsId: 'docker hub repository',passwordVariable: 'PASS', usernameVariable: 'USER')]){
+    sh 'docker build -t cnwagba/jenkins-repo-dockerhub:$IMAGE_NAME .'  //Build Docker Image
+    sh 'echo $PASS | docker login -u $USER --password-stdin'       // Authentication
+    sh 'docker push cnwagba/jenkins-repo-dockerhub:$IMAGE_NAME'       //Push to Docker repository
+    }
 }
