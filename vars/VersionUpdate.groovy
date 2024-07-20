f0e1ab07d8788a208d4885da2638771137e03db5
Chinenye Nwagbara <chinenye.nw@gmail.com>
2024-07-20 07:19:07 -0500

    Update VersionUpdate.groovy

--- vars/VersionUpdate.groovy
+++ vars/VersionUpdate.groovy
@@ -1,1 +1,9 @@
+#!/user/bin/env groovy
 
+def call(){
+    dir("app") {
+    sh "npm version patch"  //update the package.json version
+    def Version = readJSON(file: 'package.json').version
+    env.IMAGE_NAME ="$Version-$BUILD_NUMBER"                                    
+    }
+}
