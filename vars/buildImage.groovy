3d3f542c677ac4cc6031f622756ed44f0a5fe404
Chinenye Nwagbara <chinenye.nw@gmail.com>
2024-07-22 12:27:37 -0500

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,8 +1,8 @@
 def call(){
-    echo 'building the docker image...
+    echo 'building the docker image...'
     withCredentials([usernamePassword(credentialsId: 'docker hub repository',passwordVariable: 'PASS', usernameVariable: 'USER')]){
-    sh 'docker build -t cnwagba/jenkins-repo-dockerhub:$IMAGE_NAME .'  //Build Docker Image
-    sh 'echo $PASS | docker login -u $USER --password-stdin'       // Authentication
-    sh 'docker push cnwagba/jenkins-repo-dockerhub:$IMAGE_NAME'       //Push to Docker repository
+        sh 'docker build -t cnwagba/jenkins-repo-dockerhub:$IMAGE_NAME .'  //Build Docker Image
+        sh 'echo $PASS | docker login -u $USER --password-stdin'       // Authentication
+        sh 'docker push cnwagba/jenkins-repo-dockerhub:$IMAGE_NAME'       //Push to Docker repository
     }
 }
