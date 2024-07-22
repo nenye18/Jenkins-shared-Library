40b2ad1779b1b79fcc8e303575babeb800c74c0c
Chinenye Nwagbara <chinenye.nw@gmail.com>
2024-07-22 12:26:24 -0500

    Update CommitToGit.groovy

--- vars/CommitToGit.groovy
+++ vars/CommitToGit.groovy
@@ -2,11 +2,11 @@
 def call(){
     echo 'commit new version to git'
     withCredentials([usernamePassword(credentialsId: 'GitHub credentials',passwordVariable: 'PASS', usernameVariable: 'USER')]){
-    sh 'git config --global user.email "chinenye.nw@gmail.com" '
-    sh 'git config --global user.name "nenye18" '
-    sh 'git remote set-url origin https://$USER:$PASS@github.com/nenye18/Jenkins-project.git'
-    sh 'git add .'
-    sh 'git commit -m "commiting version update from jenkins CI/CD" '
-    sh 'git push origin HEAD:main'
+        sh 'git config --global user.email "chinenye.nw@gmail.com" '
+        sh 'git config --global user.name "nenye18" '
+        sh 'git remote set-url origin https://$USER:$PASS@github.com/nenye18/Jenkins-project.git'
+        sh 'git add .'
+        sh 'git commit -m "commiting version update from jenkins CI/CD" '
+        sh 'git push origin HEAD:main'
     }
   }
